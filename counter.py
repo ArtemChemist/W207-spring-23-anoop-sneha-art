b776367ea486b89b1bcbfd8252ec44360a1f153d
Artem <lebedevfedora@gmail.com>
2021-12-21 12:37:38 +0300

    Added circular mask via ogrid

--- counter.py
+++ counter.py
@@ -9,29 +9,33 @@ from os import listdir
 from os.path import isfile, join
 folder_path = os.path.dirname(__file__)+'/Smpl_Im'
 processed_path = os.path.dirname(__file__)+'/Thresholded'
-onlyfiles = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-'''
-#Read background image
-bckg = cv2.imread(folder_path+'/bckg.jpg',cv2.IMREAD_GRAYSCALE)
+file_names = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
 
-#Create mask for a circle in the center of the image area
-H, W = bckg.shape
-mask = np.full((H,W), 0, dtype=np.uint8)
-radius = W/4
-for i in range(H):
-    for k in range(W):
-        Is_In_ROI = (i-H/2)**2 + (k-W/2)**2 < radius**2
-        mask[i,k] = Is_In_ROI
+# finction creates a circular mask based on the dimentions, radius and the desired loaction
+#I modified it from here:
+#https://stackoverflow.com/questions/44865023/how-can-i-create-a-circular-mask-for-a-numpy-array
 
-#Apply mask 
-res = bckg[mask]
+def create_circular_mask(h, w, center=None, radius=None):
 
-#Find the threshold as 5x the average of the background
-thr = 5*np.median(res)
-'''
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+    
+    #Compute square of the radius to avoid computing sqrt on every step
+    radius_sq = radius**2
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center_sq = (X - center[0])**2 + (Y-center[1])**2
+
+    mask = dist_from_center_sq <= radius_sq
+    return mask
+
+#Everything below T1 is background, everything above T2 is colony
 T1 = 60
 T2 = 100
-for file in onlyfiles:
+
+for file in file_names:
     #Read the image images
     img = cv2.imread(folder_path+'/'+file)
 
@@ -41,14 +45,17 @@ for file in onlyfiles:
     #Filter out dust and pepper
     img_blur = cv2.medianBlur(img_gray,17)
 
+    #Define the center and the radius of the ROI
     H, W = img_blur.shape
-    radius_sq = (0.34*W)**2
+    radius= 0.34*W
+
+    #Set everything outside of the ROI to 0
+    circ_mask = create_circular_mask(H, W, (H/2, W/2), radius)
+    img_blur[~circ_mask] = 0
+
+    #Do the hysteresis threshold
     for i in range(1,H-1):
         for k in range(1,W-1):
-            #Crop out the LEDs
-            if (i-H/2)**2 + (k-W/2)**2 > radius_sq:
-                img_blur[i,k] = 0
-
             #Suppress low pixels
             if img_blur[i,k] < T1:
                 img_blur[i,k] = 0
@@ -58,7 +65,7 @@ for file in onlyfiles:
                 img[i,k,1] = 255
                 img_blur[i,k] = 255
             
-            #Check surrounding if in doubt
+            #If any of neigbour pixels is positive, set this to positive
             else:
                 if (   img_blur[i-1,k-1] > T2 or
                        img_blur[i,k-1] > T2 or
@@ -71,16 +78,14 @@ for file in onlyfiles:
                     ):
                     img[i,k,1] = 255
                     img_blur[i,k] = 255
-
+                #If no neigbour pixel is positive, set this to negative
                 else:
                     img_blur[i,k] = 0
 
-                
-    #thr_image = img_gray[:,:]<50
-    #img_gray[thr_image] = 0
-
+    '''            
     #Apply dynamic thresholding
-    #niBlack = cv2.ximgproc.niBlackThreshold(img_blur, 255, cv2.THRESH_BINARY, 101,0.7,cv2.ximgproc.BINARIZATION_NIBLACK)
-    #Write the final image
+    niBlack = cv2.ximgproc.niBlackThreshold(img_blur, 255, cv2.THRESH_BINARY, 101,0.7,cv2.ximgproc.BINARIZATION_NIBLACK)
+    '''
 
+    #Write the final image
     cv2.imwrite(processed_path+'/'+file, img)
