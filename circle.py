bb3fb60c819f8b1aea938c09666c8e1d9833601f
Artem Lebedev <lebedevfedora@gmail.com>
2023-04-11 23:27:28 -0400

    Fixes of pre-processing

--- circle.py
+++ circle.py
@@ -13,7 +13,7 @@ def Intensity(image = np.array, center = (int,int), radius = int, band_width = i
     h = image.shape[0] # Image height
     w = image.shape[1] # Image width
     Y, X = np.ogrid[:h, :w]
-    dist_from_center_sq = (Y - center[1])**2 + (X-center[0])**2
+    dist_from_center_sq = (Y - center[1])**2 + (X-center[0])**2 #We get x,y coord of center, NOT y,x
     mask = (dist_from_center_sq >= in_radius_sq ) & (dist_from_center_sq <= out_radius_sq)
     my_mask = np.asarray(mask, dtype="uint8")
     hist = cv2.calcHist([image], [0], my_mask, [128], [0,256])
@@ -74,36 +74,28 @@ def Find_Best_Circle(circles, image):
     '''
     Find the circle that had the sharpest change brightness as we move away from the center
     '''
-    #Take blue channel
+    #Take blue channel, threshold the hell out of it, to leave only LED
     img_gr = cv2.adaptiveThreshold(image[:,:,0],255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,31,12)
     img_gr = cv2.bitwise_not(img_gr)
 
     #Lets expand array of circles to store additional infor we find out
-    #[0] - x, [1] - y, [2] - radius, [3] - MaxRadius, [4] - MaxDeriv
+    #[0] - x, [1] - y, [2] - radius, [3], [4] - MaxDeriv, [5] - Rad for max deriv,
     zeros = np.zeros((circles.shape[0], 3), dtype = int)
-    circles = np.concatenate(  (circles, zeros),  axis = 1)  
+    circles = np.concatenate(  (circles, zeros),  axis = 1)
 
+    Deriv_Step = 10
+    Start_rad = 100
     for i, circle in enumerate(circles):
-        #Find the max radius that can possibly be at this cneter point
-        #For this find how far away this point is from the center of the image
-        #Keep in mind that image.shape[height, width], but circle[x,y,r]
-        Y_offset = abs(image.shape[0]/2-circle[0])
-        X_offset = abs(image.shape[1]/2-circle[1])
-        max_X = image.shape[1]//2-X_offset
-        max_Y = image.shape[0]//2-Y_offset
-        max_R = min(max_X, max_Y)
-        circle[3]=max_R
-        if max_R > 510:
-            #Calculate how intensity changes (i.e. derivative) as the radius increases.       
-            Deriv_Step = 10
+        #Calculate how intensity changes (i.e. derivative) as the radius increases.       
+        Deriv_f_R = Deriv(img_gr,  (circle[0], circle[1]), Start_rad, 800, Deriv_Step)
 
-            Deriv_f_R = Deriv(img_gr,  (circle[0], circle[1]), 500, 720, Deriv_Step)
-            #Find what was the sharpest change for this circle, 
-            MaxDeriv = np.max(Deriv_f_R)
-            circle[4] =  int(MaxDeriv * 1000) # we strore it in int array
-            #Find where this sharp change occured
-            R_of_Max = 500 + (Deriv_f_R.argmax()+1)*Deriv_Step
-            circle[5] = R_of_Max
+        #Find what was the sharpest change for this circle, 
+        MaxDeriv = np.max(Deriv_f_R)
+        circle[4] =  int(MaxDeriv * 1000) # we strore it in int array
+
+        #Find where this sharp change occured
+        R_of_Max = Start_rad + (Deriv_f_R.argmax()+1)*Deriv_Step
+        circle[5] = R_of_Max
 
     #Find the circle that had the most abrupt change.
     #The idea is that as we go out from the most central point will have all LEDs come into view at once
@@ -113,23 +105,23 @@ def Find_Best_Circle(circles, image):
     BestCircle = sorted_circles[-1]
 
     # Now that we know where is the true center of the ROI, let's find its true radius.
-    # Get full Deriv for this one circle, all the way to the max possible radius
+    # Get full Deriv and Intensity for this one circle
     #  
     FineStep = 2
-    Fine_Intensity = [   Intensity(img_gr, (BestCircle[0], BestCircle[1]), i, FineStep)    for i in range(500, BestCircle[3], FineStep) ]
-    Deriv_Brightest = Deriv(img_gr,  (BestCircle[0], BestCircle[1]), 500, BestCircle[3], FineStep )
+    best_center = (BestCircle[0], BestCircle[1])
+    Fine_Intensity = [   Intensity(img_gr, best_center , i, FineStep)    for i in range(Start_rad, 750, FineStep) ]
+    Deriv_Brightest = Deriv(img_gr, best_center, Start_rad, 750, FineStep )
     Fine_Int = np.round(Fine_Intensity,2)
     Fine_Deriv = np.round(Deriv_Brightest, 2)
-    # print(' ')
-    # print(Fine_Int)
-    # print(Fine_Deriv)
+    print(' ')
+    print(Fine_Int)
+    print(Fine_Deriv)
 
     # Start forming the list we will return: add center coordinate
     return_value = [BestCircle[0], BestCircle[1]]
 
-
-    # Go through the deriv and if the point and the next point is above 1500, say that is where ROI stops
-    # Don't forget that we only calculate starting from 500 pixels from the center, so add those 500px
+    # Go through the deriv and if it crosse some pre-defined trhreshold
+    # Add Start_rad pixels from the center, we only started there, not at 0
     for i in range(len(Fine_Int)-1):
         Int_crossed = False
         Deriv_crossed = False
@@ -138,7 +130,9 @@ def Find_Best_Circle(circles, image):
         if Fine_Deriv[i] > 0.09:
             Deriv_crossed = True
         if Deriv_crossed | Int_crossed:
-            return_value.append(500+i*FineStep)
+            best_rad = Start_rad+i*FineStep
+            print(f"Crossed at {best_rad }")
+            return_value.append(best_rad )
             break
 
     
